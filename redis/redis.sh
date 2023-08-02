aab4c08527a8d376231baab40b0830cca110a953
lixinhan <lixinhan@lixinhan.com>
2023-08-02 11:40:12 +0800

    redis shell

---
+++ redis/redis.sh
@@ -0,0 +1,14 @@
+#版本
+REDIS_VERSION=latest
+#外漏端口
+REDIS_PORT=6379
+#容器名称
+CONTAINER_NAME=redis-${REDIS_VERSION}
+#镜像名称
+IMAGE_NAME=redis:${REDIS_VERSION}
+
+docker run \
+-d \
+--name ${CONTAINER_NAME} \
+-p ${REDIS_PORT}:6379 \
+${IMAGE_NAME}
